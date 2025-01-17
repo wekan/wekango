a8337d7da5710594fe2b7749c9e1c6b564d1c641
Lauri Ojansivu <x@xet7.org>
2025-01-18 01:08:26 +0200

    Updated test.

--- test/test.sh
+++ test/test.sh
@@ -1,2 +1,5 @@
+# Testing local webserver with DDoS-level traffic, does webserver handle it and work OK, or crash.
+# Do not use this to test any Internet server, it is illegal, police may contact you.
+
 wrk -t12 -c100 -d300s http://127.0.0.1:8000/
 #wrk -t12 -c100 -d30s http://127.0.0.1:7000/sign-in
